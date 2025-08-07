2c4445dae9651eeeaa52e9c6160d9843b88f8272
MasterP87 <pillert.marcel87@gmail.com>
2025-08-07 21:07:14 +0200

    feat: initialisiere Express-Server-Grundstruktur

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-// Entrypoynt Füri Server her
+// Express Server basis
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send("Schulden-Manager App laef");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started auf port ${PORT}`);
+});
