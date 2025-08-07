1c3cd702a78f7a8db0d99876abd44c3c2b4b5353
MasterP87 <pillert.marcel87@gmail.com>
2025-08-07 21:10:09 +0200

    feat: Schuldner-Erstellung mit Passwort & QR-Code

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,43 @@
-// Express Server basis
+// Express Server ink. Schuldner Anlegen mit QA+Passwort
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const lowdb = require('lowdb');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const qrcode = require('qrcode');
 
 const app = express();
 const PORT = 3000;
+const db = lowdb.default(filename(path.join(__dirname, 'db.json')));
 
 app.use(bodyParser.json());
 
+app.post("/api/debtors", async (req, res) => {
+  const { name, password } = req.body;
+  const id = uuid.v4();
+  const hash = await bcrypt.hash(password, 10);
+  const record = {
+    id,
+    name,
+    password: hash,
+    balance: 0
+  };
+  db.data.debtors.push(record);
+
+  const url = `http://localhost:3000/debtor/view/${id}`;
+  const qr = await qrcode.toDataURL(url);
+
+  res.send({
+    ...record,
+    qrCode: qr
+  });
+});
 
 app.get('/', (req, res) => {
-  res.send("Schulden-Manager App laef");
+  res.send("Schulden-Manager App");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started auf port ${PORT}`);
+  console.log(`Server startet auf port ${PORT}`);
 });
